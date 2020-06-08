79955fe2156f64a719b2d3de84ed44e2553e1d9a
Yasmine Bogaert <yasmine.bogaert@hotmail.com>
2020-06-08 21:17:20 +0200

    big refactor, params through Runner()

--- code/chrome-dino/codepen/index.js
+++ code/chrome-dino/codepen/index.js
@@ -10,7 +10,10 @@
      * @constructor
      * @export
      */
-    function Runner(outerContainerId, opt_config) {
+    function Runner(outerContainerId, opt_param_config, opt_runner_config) {
+
+      this.parameters = new Parameters(opt_param_config);
+      this.parameters.initialise();
       // Singleton
       if (Runner.instance_) {
         return Runner.instance_;
@@ -19,7 +22,7 @@
       this.outerContainerEl = document.querySelector(outerContainerId);
       this.containerEl = null;
       this.snackbarEl = null;
-      this.config = opt_config || Runner.config;
+      this.config = opt_runner_config || Runner.config;
       // Logical dimensions of the container.
       this.dimensions = Runner.defaultDimensions;
       this.canvas = null;
@@ -49,7 +52,6 @@
       // Images.
       this.images = {};
       this.imagesLoaded = 0;
-      //this.parameters = new Parameters();
       if (this.isDisabled()) { // false - hardcoded
         this.setupDisabledRunner();
       } else {
@@ -57,6 +59,7 @@
       }
     }
     window['Runner'] = Runner;
+    Runner.parameters = new Parameters();
     /**
      * Default game width.
      * @const
@@ -455,6 +458,7 @@
        * Update the game frame and schedules the next one.
        */
       update: function() {
+        console.log("update - speed:", this.currentSpeed);
         this.updatePending = false;
         var now = getTimeStamp();
         var deltaTime = now - (this.time || now);
@@ -497,7 +501,7 @@
           }
     
           // Night mode.
-          if(Parameters.default_config.NIGHT_MODE_ENABLED){
+          if(this.parameters.isNightModeEnabled()){
             if (this.invertTimer > this.config.INVERT_FADE_DURATION) {
               this.invertTimer = 0;
               this.invertTrigger = false;
@@ -999,11 +1003,101 @@
 
     function Parameters(opt_config){
       this.config = opt_config ? opt_config : Parameters.default_config;
-      console.log(this.config);
     }
-    window['Parameters'] = Parameters;
 
     Parameters.prototype = {
+      initialise(){
+        this.generateObstacleTypes();
+        this.setParameters();
+      },
+      generateObstacleTypes(){
+        var minGap_default = this.getMinGap();
+        /** declarations of possible obstacle types */
+        var CACTUS_SMALL = {
+          type: 'CACTUS_SMALL',
+          width: 17,
+          height: 35,
+          yPos: 105,
+          multipleSpeed: 4,
+          minGap: minGap_default, //120,
+          minSpeed: 0,
+          collisionBoxes: [
+            new CollisionBox(0, 7, 5, 27),
+            new CollisionBox(4, 0, 6, 34),
+            new CollisionBox(10, 4, 7, 14)
+          ]
+        }
+
+        var CACTUS_LARGE = {
+          type: 'CACTUS_LARGE',
+          width: 25,
+          height: 50,
+          yPos: 90,
+          multipleSpeed: 7,
+          minGap: minGap_default, //120,
+          minSpeed: 0,
+          collisionBoxes: [
+            new CollisionBox(0, 12, 7, 38),
+            new CollisionBox(8, 0, 7, 49),
+            new CollisionBox(13, 10, 10, 38)
+          ]
+        }
+
+        var PTERODACTYL = {
+          type: 'PTERODACTYL',
+          width: 46,
+          height: 40,
+          yPos: this.getPterodactylYPOS(), //[ 100, 75, 50 ], // Variable height.
+          yPosMobile: [ 100, 50 ], // Variable height mobile.
+          multipleSpeed: 999,
+          minSpeed: 8.5,
+          minGap: minGap_default + 30, //150,
+          collisionBoxes: [
+            new CollisionBox(15, 15, 16, 5),
+            new CollisionBox(18, 21, 24, 6),
+            new CollisionBox(2, 14, 4, 3),
+            new CollisionBox(6, 10, 4, 7),
+            new CollisionBox(10, 8, 6, 9)
+          ],
+          numFrames: 2,
+          frameRate: 1000/6,
+          speedOffset: .8
+        }
+        Obstacle.types = []
+          
+        /* dynamically used obstacletypes */
+        var obstacleTypes = this.getObstacleTypes();
+        var nr_ObstacleTypes = obstacleTypes.length;
+        for(var i = 0; i < nr_ObstacleTypes; i++){
+          var type = obstacleTypes[i];
+          switch(type){
+            case 'CACTUS_SMALL':
+              Obstacle.types.push(CACTUS_SMALL);
+              break;
+            case 'CACTUS_LARGE':
+              Obstacle.types.push(CACTUS_LARGE);
+              break;
+            case 'PTERODACTYL':
+              if(nr_ObstacleTypes == 1){
+                PTERODACTYL.minSpeed = 0;
+              } else if (nr_ObstacleTypes == 2){
+              Runner.config.MAX_OBSTACLE_DUPLICATION = 7;
+              }
+              Obstacle.types.push(PTERODACTYL)
+              break;
+          }
+        }
+
+        if(nr_ObstacleTypes == 1){
+          Runner.config.MAX_OBSTACLE_DUPLICATION = Number.MAX_SAFE_INTEGER;
+        }
+        
+        /**
+         * 3 types : max obstacle duplication = 2 (default)
+         * 2 types : max obstacle duplication = 7
+         * 1 type  : max obstacle duplication = infinite
+         */
+      },
       setParameters: function(){
         /**
          * Overwrite necessary parameters:
@@ -1014,15 +1108,45 @@
          * - Obstacle.MAX_OBSTACLE_LENGTH
          * TODO: update all parameters in this function, instead of hardcoded
          */
-        Runner.config.SPEED = this.config.SPEED;
-        Runner.config.ACCELERATION = this.config.ACCELERATION;
-        Runner.config.INVERT_DISTANCE = this.config.NIGHT_MODE_DISTANCE;
-        Runner.config.CLEAR_TIME = this.config.CLEAR_TIME;
-        Runner.config.MAX_SPEED = this.config.MAX_SPEED;
-        Obstacle.MAX_OBSTACLE_LENGTH = this.config.MAX_OBSTACLE_LENGTH;
-        minGap_default = this.config.MIN_GAP;
+        Runner.config.SPEED = this.getSpeed();
+        Runner.config.ACCELERATION = this.getAcceleration();
+        Runner.config.INVERT_DISTANCE = this.getNightModeDistance();
+        Runner.config.CLEAR_TIME = this.getClearTime();
+        Runner.config.MAX_SPEED = this.getMaxSpeed();
+        Obstacle.MAX_OBSTACLE_LENGTH = this.getMaxObstacleLength();
+      },
+      getSpeed(){
+        return this.config.SPEED;
+      },
+      getAcceleration(){
+        return this.config.ACCELERATION;
+      },
+      getMinGap(){
+        return this.config.MIN_GAP;
+      },
+      getObstacleTypes(){
+        return this.config.OBSTACLE_TYPES;
+      },
+      isNightModeEnabled(){
+        return this.config.NIGHT_MODE_ENABLED;
+      },
+      getNightModeDistance(){
+        return this.config.NIGHT_MODE_DISTANCE;
+      },
+      getClearTime(){
+        return this.config.CLEAR_TIME;
+      },
+      getMaxObstacleLength(){
+        return this.config.MAX_OBSTACLE_LENGTH;
+      },
+      getMaxSpeed(){
+        return this.config.MAX_SPEED;
+      },
+      getPterodactylYPOS(){
+        return this.config.PTERODACTYL_YPOS;
       }
     }
+    //var parameters = new Parameters();
 
     /**
      * Logging module
@@ -1475,6 +1599,7 @@
      * speedOffset: speed faster / slower than the horizon.
      * minSpeed: Minimum speed which the obstacle can make an appearance.
      */
+    /*
     var minGap_default = (typeof minGap_default == 'undefined' ? 120 : minGap_default);
     var CACTUS_SMALL = {
       type: 'CACTUS_SMALL',
@@ -1510,7 +1635,7 @@
       type: 'PTERODACTYL',
       width: 46,
       height: 40,
-      yPos: Parameters.default_config.PTERODACTYL_YPOS, //[ 100, 75, 50 ], // Variable height.
+      yPos: Runner.parameters.getPterodactylYPOS(), //[ 100, 75, 50 ], // Variable height.
       yPosMobile: [ 100, 50 ], // Variable height mobile.
       multipleSpeed: 999,
       minSpeed: 8.5,
@@ -1527,7 +1652,9 @@
       speedOffset: .8
     }
     Obstacle.types = []
-    
+    */
+
+
     /*Obstacle.types = [
        {
         type: 'CACTUS_SMALL',
@@ -1578,9 +1705,11 @@
         speedOffset: .8
       }
     ];*/
-    var nr_ObstacleTypes = Parameters.default_config.OBSTACLE_TYPES.length;
+   
+   /* var obstacleTypes = parameters.getObstacleTypes();
+    var nr_ObstacleTypes = obstacleTypes.length;
     for(var i = 0; i < nr_ObstacleTypes; i++){
-      var type = Parameters.default_config.OBSTACLE_TYPES[i];
+      var type = obstacleTypes[i];
       switch(type){
         case 'CACTUS_SMALL':
           Obstacle.types.push(CACTUS_SMALL);
@@ -1589,9 +1718,9 @@
           Obstacle.types.push(CACTUS_LARGE);
           break;
         case 'PTERODACTYL':
-          if(Parameters.default_config.OBSTACLE_TYPES.length == 1){
+          if(nr_ObstacleTypes == 1){
             PTERODACTYL.minSpeed = 0;
-          } else if (Parameters.default_config.OBSTACLE_TYPES.length == 2){
+          } else if (nr_ObstacleTypes == 2){
           Runner.config.MAX_OBSTACLE_DUPLICATION = 7;
           }
           Obstacle.types.push(PTERODACTYL)
@@ -1601,7 +1730,7 @@
 
     if(nr_ObstacleTypes == 1){
       Runner.config.MAX_OBSTACLE_DUPLICATION = Number.MAX_SAFE_INTEGER;
-    }
+    }*/
 
     //******************************************************************************
     /**
@@ -2671,10 +2800,19 @@
     
     
     function onDocumentLoad() {
-        r = new Runner('.interstitial-wrapper');
-        p = new Parameters();
-        p.setParameters();
-        // r.updateConfigSetting("SPEED",10);
+      var par = {
+        SPEED: 7, //
+        ACCELERATION: 0.2,  //
+        MIN_GAP: 120,
+        OBSTACLE_TYPES: [ 'CACTUS_LARGE', 'CACTUS_SMALL', 'PTERODACTYL'], //
+        NIGHT_MODE_ENABLED: true, //
+        NIGHT_MODE_DISTANCE: 100, //
+        CLEAR_TIME: 0, //
+        MAX_OBSTACLE_LENGTH: 1, //
+        MAX_SPEED: 12, //
+        PTERODACTYL_YPOS: [ 100, 75, 50 ], //
+      }
+      r = new Runner('.interstitial-wrapper', par);
 
     }
     
