669cfd0ea3798283639637d6e0a1ac382dacb2c8
Yasmine Bogaert <yasmine.bogaert@hotmail.com>
2020-06-16 19:03:26 +0200

    collisionbox

---
+++ code/collisionbox.js
@@ -0,0 +1,14 @@
+//******************************************************************************
+/**
+ * Collision box object.
+ * @param {number} x X position.
+ * @param {number} y Y Position.
+ * @param {number} w Width.
+ * @param {number} h Height.
+ */
+function CollisionBox(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+};
