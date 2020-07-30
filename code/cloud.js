c62df650b6de071f59c02ecd4a1ecb1d64efbaca
Yasmine Bogaert <yasmine.bogaert@hotmail.com>
2020-07-30 10:28:58 +0200

    refactor Cloud into class

--- code/cloud.js
+++ code/cloud.js
@@ -6,43 +6,46 @@
  * @param {Object} spritePos Position of image in sprite.
  * @param {number} containerWidth
  */
-function Cloud(canvas, spritePos, containerWidth) {
-    this.canvas = canvas;
-    this.canvasCtx = this.canvas.getContext('2d');
-    this.spritePos = spritePos;
-    this.containerWidth = containerWidth;
-    this.xPos = containerWidth;
-    this.yPos = 0;
-    this.remove = false;
-    this.cloudGap = getRandomNum(Cloud.config.MIN_CLOUD_GAP,
-        Cloud.config.MAX_CLOUD_GAP);
-    this.init();
-};
-/**
- * Cloud object config.
- * @enum {number}
- */
-Cloud.config = {
-    HEIGHT: 14,
-    MAX_CLOUD_GAP: 400,
-    MAX_SKY_LEVEL: 30,
-    MIN_CLOUD_GAP: 100,
-    MIN_SKY_LEVEL: 71,
-    WIDTH: 46
-};
-Cloud.prototype = {
+class Cloud {
+
+    /**
+     * Cloud object config.
+     * @enum {number}
+     */
+    static config = {
+        HEIGHT: 14,
+        MAX_CLOUD_GAP: 400,
+        MAX_SKY_LEVEL: 30,
+        MIN_CLOUD_GAP: 100,
+        MIN_SKY_LEVEL: 71,
+        WIDTH: 46
+    }
+
+    constructor(canvas, spritePos, containerWidth) {
+        this.canvas = canvas;
+        this.canvasCtx = this.canvas.getContext('2d');
+        this.spritePos = spritePos;
+        this.containerWidth = containerWidth;
+        this.xPos = containerWidth;
+        this.yPos = 0;
+        this.remove = false;
+        this.cloudGap = getRandomNum(Cloud.config.MIN_CLOUD_GAP,
+            Cloud.config.MAX_CLOUD_GAP);
+        this.init();
+    }
+
     /**
      * Initialise the cloud. Sets the Cloud height.
      */
-    init: function () {
+    init(){
         this.yPos = getRandomNum(Cloud.config.MAX_SKY_LEVEL,
             Cloud.config.MIN_SKY_LEVEL);
         this.draw();
-    },
+    }
     /**
      * Draw the cloud.
      */
-    draw: function () {
+    draw(){
         this.canvasCtx.save();
         var sourceWidth = Cloud.config.WIDTH;
         var sourceHeight = Cloud.config.HEIGHT;
@@ -56,12 +59,12 @@ Cloud.prototype = {
             this.xPos, this.yPos,
             Cloud.config.WIDTH, Cloud.config.HEIGHT);
         this.canvasCtx.restore();
-    },
+    }
     /**
      * Update the cloud position.
      * @param {number} speed
      */
-    update: function (speed) {
+    update(speed){
         if (!this.remove) {
             this.xPos -= Math.ceil(speed);
             this.draw();
@@ -70,12 +73,12 @@ Cloud.prototype = {
                 this.remove = true;
             }
         }
-    },
+    }
     /**
      * Check if the cloud is visible on the stage.
      * @return {boolean}
      */
-    isVisible: function () {
+    isVisible(){
         return this.xPos + Cloud.config.WIDTH > 0;
     }
-};
+}
